f86f5ebcc5917320a3deb8ac0eb8462a27284a59
SybrenBootsma <47575390+SybrenBootsma@users.noreply.github.com>
2019-03-20 10:58:31 +0100

    Update Velocity_calc.py

--- Velocity_calc.py
+++ Velocity_calc.py
@@ -16,20 +16,14 @@ def velocity(V_IAS,hp,Tm):
     R = 287.057
     g0= 9.80665
     gamma = 1.4
-    Tm = Tm + 273.15
+    TAT = Tm + 273.15
     hp = hp * 0.3048
     Vc = (V_IAS - 2)*0.514444
     p = p0 * (1 + (lapse * hp)/T0)**(-g0/(lapse*R))
     M = np.sqrt(2/(gamma - 1) * ((1+(p0/p)*((1+(gamma-1)/(2*gamma) * (rho0/p0)*Vc**2.)**(gamma/(gamma-1)) - 1))**((gamma - 1)/gamma) - 1))
-    T = Tm / (1 + (gamma-1)/2 * M*M)
+    T = TAT / (1 + (gamma-1)/2 * M*M)
     a = np.sqrt(gamma*R*T)
     Vt = M*a
     rho = rho0 * (T/T0)**(-(g0/(lapse*R) + 1))
     Ve = Vt * np.sqrt(rho/rho0)
-    return Vc, M, T, a, Vt, Ve, rho
-
-#hp1 = np.array([5010, 5020, 5020, 5030, 5020, 5110]) #Pressure Altitude in ft
-#IAS1 = np.array([249, 221, 192, 163, 130, 118]) #Indicated Airspeed in knots
-#TAT1 = np.array([12.5, 10.5, 8.8, 7.2, 6, 5.2]) #Total air temperature in Celsius
-#
-#v = velocity(IAS1,hp1,TAT1)
+    return Vc, M, a, Vt, Ve, rho
