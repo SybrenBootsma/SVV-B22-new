3a3aa4252c9db9296e94d2ad7d166ff21f06aeb4
SybrenBootsma <47575390+SybrenBootsma@users.noreply.github.com>
2019-03-20 10:37:33 +0100

    Update Velocity_calc.py

--- Velocity_calc.py
+++ Velocity_calc.py
@@ -7,15 +7,15 @@ Created on Tue Mar 12 15:31:51 2019
 from math import *
 import numpy as np
 
-p0 = 101325             #Pa ISA
-rho0 = 1.225            #Kg/m3
-lapse = -6.5 * 10**(-3)     #deg C/m
-T0 = 288.15
-R = 287.057
-g0= 9.80665
-gamma = 1.4    
    
 def velocity(V_IAS,hp,Tm):
+    p0 = 101325             #Pa ISA
+    rho0 = 1.225            #Kg/m3
+    lapse = -6.5 * 10**(-3)     #deg C/m
+    T0 = 288.15
+    R = 287.057
+    g0= 9.80665
+    gamma = 1.4
     Tm = Tm + 273.15
     hp = hp * 0.3048
     Vc = (V_IAS - 2)*0.514444
@@ -27,9 +27,3 @@ def velocity(V_IAS,hp,Tm):
     rho = rho0 * (T/T0)**(-(g0/(lapse*R) + 1))
     Ve = Vt * np.sqrt(rho/rho0)
     return Vc, M, a, Vt, Ve, rho
-
-hp = np.array([5010, 5020, 5020, 5030, 5020, 5110]) #Pressure Altitude in ft
-V_IAS = np.array([249, 221, 192, 163, 130, 118]) #Indicated Airspeed in knots
-Tm = np.array([12.5, 10.5, 8.8, 7.2, 6, 5.2]) #Total air temperature in Celsius
-
-v = velocity(V_IAS, hp, Tm)
