e863afd8c0e25bd6b29c4bf0d606ee0bea80de00
Tim Burgers <tim-burgers@hotmail.com>
2019-03-26 14:37:16 +0100

    Update Cit_par.py

--- Cit_par.py
+++ Cit_par.py
@@ -4,12 +4,12 @@ from Ref_data import *
 
 #symmetric
 
-mass, hp0, Vt0, alpha0, th0, delta_e0 = Short_period_init()
-time, pitch_rate, u_s, alpha, pitch, u = Short_period()
+#mass, hp0, Vt0, alpha0, th0, delta_e0 = Short_period_init()
+#time, pitch_rate, u_s, alpha, pitch, u = Short_period()
 # ,roll0, roll_rate0, yaw_rate0
 #assymetric
-#mass, hp0, Vt0, alpha0, th0, delta_r0, delta_a0= Spiral_init()
-#time, delta_r, delta_a, beta, roll, roll_rate, yaw_rate = Spiral()
+mass, hp0, Vt0, alpha0, th0, delta_r0, delta_a0= Dutch_roll_init()
+time, delta_r, delta_a, beta, roll, roll_rate, yaw_rate = Dutch_roll()
 
 
 
