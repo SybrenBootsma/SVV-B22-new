cc665dd1495d3a4576a96b4d5768e4dbde9bf928
Max Knoop <maxknoop200@gmail.com>
2019-03-27 11:38:30 +0100

    Update Cit_par_Our_data.py

--- Cit_par_Our_data.py
+++ Cit_par_Our_data.py
@@ -6,16 +6,16 @@ from Test_data_outputs import *
 #mass, hp0, Vt0, alpha0, th0, delta_e0 = Pheugoid_init()
 #time, pitch_rate, u_s, alpha, pitch, u = Pheugoid()
 
-#mass, hp0, Vt0, alpha0, th0, delta_e0 = Short_period_init()
-#time, pitch_rate, u_s, alpha, pitch, u = Short_period()
+mass, hp0, Vt0, alpha0, th0, delta_e0 = Short_period_init()
+time, pitch_rate, u_s, alpha, pitch, u = Short_period()
 ## 
 #assymetric
 #
 #mass, hp0, Vt0, alpha0, th0, delta_r0, delta_a0 ,roll0, roll_rate0, yaw_rate0= Spiral_init()
 #time, delta_r, delta_a, beta, roll, roll_rate, yaw_rate = Spiral()
 
-mass, hp0, Vt0, alpha0, th0, delta_r0, delta_a0 ,roll0, roll_rate0, yaw_rate0= Dutch_roll_init()
-time, delta_r, delta_a, beta, roll, roll_rate, yaw_rate = Dutch_roll()
+#mass, hp0, Vt0, alpha0, th0, delta_r0, delta_a0 ,roll0, roll_rate0, yaw_rate0= Dutch_roll_init()
+#time, delta_r, delta_a, beta, roll, roll_rate, yaw_rate = Dutch_roll()
 
 
 
